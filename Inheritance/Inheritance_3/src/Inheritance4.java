166e7daa73dae3339f2f981e8d2d4b167771a150
Emma Veletic <veletic.emma@outlook.com>
2024-03-09 17:05:43 +0100

    Inheritence exercise 3 and 4

---
+++ Inheritance/Inheritance_3/src/Inheritance4.java
@@ -0,0 +1,45 @@
+
+class Employee{
+
+    protected double salary;
+
+    Employee(double salary){
+        this.salary = salary;
+    }
+    public void work(){
+        System.out.println("Employee works");
+    }
+
+    public double getSalary(){
+        return salary;
+    }
+}
+
+class HRManager extends Employee{
+
+    HRManager(double salary){
+        super(salary);
+    }
+    @Override
+    public void work(){
+        System.out.println("HRManager works");
+    }
+
+    public void addEmployee(){
+        System.out.println("Adding employee");
+    }
+
+}
+
+
+public class Inheritance4 {
+    public static void main(String[] args) {
+        Employee employee = new Employee(600.00);
+        HRManager hrManager = new HRManager(800.00);
+        employee.work();
+        hrManager.work();
+        System.out.println(employee.getSalary());
+
+        hrManager.addEmployee();
+    }
+}
